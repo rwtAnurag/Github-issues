25d7263113abd80d4a244f4d590445d9896eec48
Anurag Rawat <hackprajjwal2025@gmail.com>
2021-04-28 22:42:33 +0530

    initial commit

---
+++ github-issues-pages/src/components/Header2.js
@@ -0,0 +1,27 @@
+
+import 'bootstrap/dist/css/bootstrap.css';
+import './header.css';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import DehazeIcon from '@material-ui/icons/Dehaze';
+import NotificationsNoneIcon from '@material-ui/icons/NotificationsNone';
+import AddIcon from '@material-ui/icons/Add';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
+const Header2 =()=>{
+    return(
+      // <div>
+      //   <h1>This is for mobile</h1>
+      // </div>
+      <header className="App-header pl-4  pb1-1" >
+       <GitHubIcon  fontSize="large"/>
+        <input  type="text" className="ml-4 mt-3 rounded-lg  pl-3 pt-1 pb-1 " aria-describedby="inputGroup-sizing-lg" placeholder="Search or jump to..."/>
+        <span className="float-right">
+          <DehazeIcon  fontSize="large">
+          </DehazeIcon>
+         </span>
+        
+      </header>
+    );
+}
+
+export default Header2;
